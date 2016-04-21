bcd33380c7fd23adb23de835c6206d2f532063db
ChrisWei <luchenfeng199211@163.com>
2016-04-21 10:41:17 +0800

    Initial Commit

---
+++ CWScanQRCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CWScanQRCode
+//
+//  Created by 陆尘风 on 16/4/21.
+//  Copyright © 2016年 陆尘风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
